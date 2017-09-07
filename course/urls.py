066cda3bf86dc4f9d0a6a3fdbd8118d435e2c26a
Sergey19940808 <aleksey.serzh@mail.ru>
2017-09-07 18:18:59 +0500

    Finish work

--- course/urls.py
+++ course/urls.py
@@ -3,12 +3,22 @@ from django.conf.urls import url
 
 from .models import Course
 
-from .views import ShowHomePage, AddCourse
+from .views import ShowHome, SelectCourse, AddCourse, EditCourse, \
+    SearchCourse, DeleteCourse
 
 # Create urls for app course
 
 urlpatterns = [
-    url(r'^$', ShowHomePage.as_view(model = Course, template_name = 'course/home_page_catalog.html'), name = 'show-home'),
-    url(r'^add_course/$', AddCourse.as_view(template_name = 'course/add_page_course.html'), name='add-course'),
-
+    url(r'^$', ShowHome.as_view(model = Course, template_name = 'course/home_page_catalog.html'),
+    name = 'show-home'),
+    url(r'^select_course/(?P<pk>\d+)/$', SelectCourse.as_view(model = Course,
+    template_name = 'course/select_page_course.html'), name = 'select-course'),
+    url(r'^add_course/$', AddCourse.as_view(template_name = 'course/add_page_course.html'),
+    name='add-course'),
+    url(r'^edit_course/(?P<pk>\d+)/$', EditCourse.as_view(model = Course, template_name = 'course/edit_page_course.html'),
+    name = 'edit-course'),
+    url(r'^search_course/$', SearchCourse.as_view(),
+    name = 'search-course'),
+    url(r'^delete_course/(?P<pk>\d+)/$', DeleteCourse.as_view(model = Course,
+    template_name = 'course/delete_page_course.html'), name = 'delete-course')
 ]
--- course/views.py
+++ course/views.py
@@ -9,16 +9,25 @@ from .models import Course
 
 from .forms import AddCourseForm
 
+from django.db.models import Q
+
 
 
 # Create views with class-base-views
-class ShowHomePage(ListView):
+class ShowHome(ListView):
     context_object_name = 'courses'
 
     def get_queryset(self):
         courses = Course.objects.all()
         return courses
 
+
+class SelectCourse(DetailView):
+    context_object_name = 'course'
+
+    def get_object(self, queryset=Course.objects.all()):
+        object = super(SelectCourse, self).get_object()
+        return object
 
 
 class AddCourse(FormView):
@@ -31,4 +40,44 @@ class AddCourse(FormView):
         form.clean_description()
         self.object.save()
         return super(AddCourse, self).form_valid(form)
+
+
+
+class EditCourse(UpdateView):
+    fields = ['name', 'description']
+    context_object_name = 'course'
+
+    def get_success_url(self, **kwargs):
+        return reverse_lazy('select-course', kwargs={'pk': self.object.id})
+
+
+
+class SearchCourse(View):
+    template_name = 'course/search_page_course.html'
+
+
+    def get(self, request):
+        query = request.GET.get('query')
+
+        if query:
+            qset = (
+                Q(name__startswith=query) |
+                Q(description__startswith=query) |
+                Q(date_added__startswith=query)
+            )
+            posts = Course.objects.filter(qset)
+
+        else:
+            posts = []
+
+        return render(request, self.template_name, {'query': query, 'posts': posts})
+
+
+
+class DeleteCourse(DeleteView):
+    context_object_name = 'course'
+    success_url = reverse_lazy('show-home')
+
+    def get_template_names(self):
+        return self.template_name
 
